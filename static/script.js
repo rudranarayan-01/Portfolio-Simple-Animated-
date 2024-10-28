60d2c72febeb07a00a34230105af8e11cd8f18aa
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2024-10-28 13:12:53 +0530

    Updated tag

--- static/script.js
+++ static/script.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  var typingEffect = new Typed(".typedText",{
-    strings : ["Web Developer","Android Developer","Freelancer"],
+    strings : ["ML Engineer","Web Developer","Android Developer","Freelancer"],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
